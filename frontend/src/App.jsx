bf3204228822167dff32938ba339438200d6249b
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-08-16 01:13:12 +0530

    #Added functionality to signup to the app and insert data to database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
@@ -9,7 +9,7 @@ import ChatPage from './pages/ChatPage';
 import OnboardingPage from './pages/OnboardingPage.jsx';
 import toast, {Toaster} from 'react-hot-toast';
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { axiosInstance } from './lib/axios.js';
 
 
 
@@ -17,28 +17,26 @@ const App = () => {
 
   //transtack query
   //axios
-  const {data, isLoading, error, refetch} = useQuery({
-    queryKey: ["todos"],
+  const {data: authData, isLoading, error} = useQuery({
+    queryKey: ["authUser"],
     queryFn: async ()=> {
-      const res = await axios.get("https://jsonplaceholder.cypress.io/todos/1")
+      const res = await axiosInstance.get("/auth/me");
       return res.data;
     },
+    retry: false, //auth check
   });
-
-  console.log({data});
-  console.log({isLoading});
-  console.log({error});
+const authUser =authData?.user
 
   return (
     <div className=' h-screen' data-theme="night" >
       <Routes>
-       <Route path="/" element={<HomePage />}/>
-       <Route path="/signup" element={<SignUpPage />}/>
-       <Route path="/login" element={<LoginPage />}/>
-       <Route path="/notification" element={<NotificationsPage />}/>
-       <Route path="/call" element={<CallPage />}/>
-       <Route path="/chat" element={<ChatPage />}/>
-       <Route path="/onboarding" element={<OnboardingPage />}/>
+       <Route path="/" element={authUser? <HomePage /> : <Navigate to={"/login"} />}/>
+       <Route path="/signup" element={!authUser?<SignUpPage />: <Navigate to="/"/>}/>
+       <Route path="/login" element={!authUser?<LoginPage />: <Navigate to= "/" />}/>
+       <Route path="/notification" element={authUser?<NotificationsPage/>: <Navigate to={"/login"} />}/>
+       <Route path="/call" element={authUser?<CallPage />:<Navigate to={"/login"} />}/>
+       <Route path="/chat" element={authUser?<ChatPage />:<Navigate to={"/login"} />}/>
+       <Route path="/onboarding" element={authUser?<OnboardingPage />: <Navigate to={"/login"} />}/>
       </Routes>
       <Toaster/>     
     </div>
