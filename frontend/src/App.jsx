a67a101c20d392286b2163ef5be5f69eb07b6cef
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-08-15 12:47:06 +0530

    #added necessary pages routes for routing properly 😁

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import { Route, Routes } from 'react-router';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+import toast, {Toaster} from 'react-hot-toast';
+
+
 
 const App = () => {
   return (
-    <div className=' h-screen text-5xl' data-theme="night" >
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className=' h-screen' data-theme="night" >
+      <Routes>
+       <Route path="/" element={<HomePage />}/>
+       <Route path="/signup" element={<SignUpPage />}/>
+       <Route path="/login" element={<LoginPage />}/>
+       <Route path="/notification" element={<NotificationsPage />}/>
+       <Route path="/call" element={<CallPage />}/>
+       <Route path="/chat" element={<ChatPage />}/>
+       <Route path="/onboarding" element={<OnboardingPage />}/>
+      </Routes>
+      <Toaster/>     
     </div>
   )
 };
