c2f23569dcc1fb2e0e1be36dda1b7bf82e9cd755
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-08-16 12:43:45 +0530

    #Added Avatar on OnboardingPage and a random avatar generator button for onboarding page

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,18 @@
+import axios from "axios";
 import { axiosInstance } from "./axios";
 
 export const signup = async (signupData) => {
     const response = await axiosInstance.post("/auth/signup", signupData);
     return  response.data;
 };
+
+
+export const getAuthUser = async ()=> {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+};
+
+export const completeOnboarding = async (userData)=> {
+    const response = await axios.post("/auth/onboarding", userData);
+    return response.data;
+}
