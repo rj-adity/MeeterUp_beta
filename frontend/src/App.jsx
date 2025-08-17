41beef479bcc7dbc8f4dac44584a3b2a83cb7866
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-08-17 21:47:42 +0530

    #Added a theme selector where u can select multiple theme and added the theme integration to default in pageLoader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { Toaster } from 'react-hot-toast';
 import PageLoader from './components/PageLoader.jsx';
 import useAuthUser from './hooks/useAuthUser.js';
 import Layout from './components/Layout.jsx';
+import { useThemeStore } from './store/useThemeStore.js';
 
 
 
@@ -22,13 +23,14 @@ const App = () => {
 
   const { isLoading, authUser } = useAuthUser();
 
+  const {theme} = useThemeStore()
+
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded
 
   if (isLoading) return <PageLoader />;
-
   return (
-    <div className=' h-screen' data-theme="forest" >
+    <div className=' h-screen' data-theme={theme} >
       <Routes>
         <Route path="/" element={isAuthenticated && isOnboarded ? (
           <Layout showSidebar={true} >
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,18 @@
 import { Link, useLocation } from "react-router"
 import useAuthUser from "../hooks/useAuthUser"
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { logout } from "../lib/api";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { logout } from "../lib/api";
 import { BellIcon, LogOutIcon, PandaIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
+import useLogout from "../hooks/useLogout";
 
 const Navbar = () => {
     const {authUser } = useAuthUser();
     const location = useLocation();
     const isChatPage = location.pathname?.startsWith("/chat");
 
-    const queryClient = useQueryClient();
-
-   const {mutate: logoutMutation} = useMutation({
-        mutationFn: logout,
-        onSuccess: () => queryClient.invalidateQueries({queryKey: ["authUser"]})
-    })
+  
+    const{logoutMutation} =  useLogout();
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center" >
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
