fa4c266ca1927d1a6833dcd7e07fadccf36dab75
rj-adity <75466680+rj-adity@users.noreply.github.com>
2025-08-18 16:52:46 +0530

    #Added a fully functional video clalling page with vieo recording option share screen and reactions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,13 @@ const App = () => {
         )
       }
         />
-        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to={"/login"} />} />
+        <Route path="/call/:id" element={
+          isAuthenticated && isOnboarded ? (
+            <CallPage />
+          ): (
+            <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+          )
+        } />
         <Route 
         path="/chat/:id" 
         element={
